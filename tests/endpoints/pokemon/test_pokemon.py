d4f8394d739f41035ab10ce33a0bf8740981fcf2
TWG <76501419+timothygaus@users.noreply.github.com>
2025-03-16 10:13:26 -0400

    Add data validation test

--- tests/endpoints/pokemon/test_pokemon.py
+++ tests/endpoints/pokemon/test_pokemon.py
@@ -62,3 +62,20 @@ def test_pokemon_id_has_expected_name(base_url, pokemon_test_cases):
     assert (
         response.json()["id"] == pokemon_id
     ), f"Expected ID {pokemon_id} but got {response.json()['id']} for name {pokemon_name}"
+
+def test_pokemon_data_has_valid_types(base_url, pokemon_test_cases):
+    """Test that various fields within pokemon contain the correct data types"""
+    pokemon_name = pokemon_test_cases["name"]
+    response = make_request("GET", f"{base_url}pokemon/{pokemon_name}")
+    assert response.status_code == 200
+    id_response = response.json()["id"] # Expect integer
+    name_response = response.json()["name"] # Expect string
+    abilities_response = response.json()["abilities"] # Expect a list of PokemonAbility
+    types_response = response.json()["types"] # Expect a list of PokemonType
+    assert isinstance(id_response, int), f"Expected 'id' to be int, but got {type(id_response)}"
+    assert isinstance(name_response, str), f"Expected 'name' to be string, but got {type(name_response)}"
+    assert isinstance(abilities_response, list), f"Expected 'abilities' to be list, but got {type(abilities_response)}"
+    assert isinstance(types_response, list), f"Expected 'types' to be list, but got {type(abilities_response)}"
+    # Check that lists are not empty
+    assert len(abilities_response) > 0, f"Expected at least one entry in 'abilities' list, but list was empty"
+    assert len(types_response) > 0, f"Expected at least one entry in 'types', list, but list was empty"
